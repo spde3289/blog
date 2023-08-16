0b681066e825f0786fd566a113bbe449f47d1be2
spde3289 <spde3289@naver.com>
2023-08-16 17:00:21 +0900

    refactor: searchContext 타입 가시성 개선

--- src/context/searchContext.tsx
+++ src/context/searchContext.tsx
@@ -1,35 +1,33 @@
 import { createContext, useState, useContext, useMemo } from "react";
 
-interface handleType {
-  input: (value: string) => void
-}
+type SearchContextType = [string, {input: (value: string) => void}]
 
 interface props {
   children: JSX.Element;
 }
 
-const searchContext = createContext<[string, handleType] | null>(null);
+const SearchContext = createContext<SearchContextType>(null!);
 
 export function SearchProvider({ children }: props) {
   const [searchValue, setSearchValue] = useState("");
 
   const handle = useMemo(()=>({
-    input: (value: string) => setSearchValue(value),
+      input: (value: string) => setSearchValue(value),
     }),
     [setSearchValue]
   )
   
-  return <searchContext.Provider children={children} value={[searchValue, handle]} />;
+  return <SearchContext.Provider children={children} value={[searchValue, handle]} />;
 }
 
 export function useSearchContext() {
-  const currentContext = useContext(searchContext);
+  const currentContext = useContext(SearchContext);
 
     if (!currentContext) {
-    throw new Error(
-      "useCurrentUser has to be used within <CurrentUserContext.Provider>"
-    );
-  }
+      throw new Error(
+        "useCurrentUser has to be used within <CurrentContext.Provider>"
+      );
+    }
 
   return currentContext;
 }
