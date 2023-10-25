0264767c0d764442655c837e98b227f4eaee0945
spde3289 <spde3289@naver.com>
2023-10-25 17:14:48 +0900

    fix: 빌드 에러 수정

--- src/pages/post/Node.js/CreateDiscordBotTwo/imgs/index.ts
+++ src/pages/post/Node.js/CreateDiscordBotTwo/imgs/index.ts
@@ -1,7 +1,7 @@
 import totalSearch  from "./종합랭킹.png";
 import result from "./결과.png";
 import change from "./로직변경.png";
-import embeds from "./Embeds.png";
+import embeds from "./embeds.png";
 import expCrawling from "./expCrawling.png"
 import charSrarch from "./charSearch.png"
 
