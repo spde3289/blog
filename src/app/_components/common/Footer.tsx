fb7a78a9f7b4502d0b6dcdc75ae24663a1987a26
spde3289 <spde3289@naver.com>
2026-02-10 22:14:31 +0900

    feat: 푸터 컴포넌트 추가

---
+++ src/app/_components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-10 text-center text-neutral-600 text-sm border-t border-neutral-900 bg-[#0a0a0a]">
+      © 2025 Developer Kim Ji-hoon. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
