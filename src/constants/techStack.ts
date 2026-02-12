a41a911a4b1a1c75a06beed2f04fb66c14d70db0
spde3289 <spde3289@naver.com>
2026-02-12 18:08:25 +0900

    chore: 기술 스택 데이터 수정

--- src/constants/techStack.ts
+++ src/constants/techStack.ts
@@ -8,11 +8,19 @@ export const TECH_STACK = {
   ],
   library: [
     "Tailwind CSS",
+    "Emotion",
     "Styled-components",
     "Zustand",
     "TanStack Query",
     "Recoil",
   ],
-  tools: ["Git & GitHub", "VS Code", "Figma", "Slack"],
-  env: ["Vercel", "AWS (EC2)", "Docker"],
+  tools: [
+    "Git & GitHub",
+    "VS Code",
+    "Figma",
+    "Notion",
+    "Husky",
+    "ESLint & Prettier",
+  ],
+  env: ["Vercel", "AWS (EC2)"],
 };
